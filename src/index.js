083a5bc56b030bf32d8b17a2ace9b4e3b9b733ad
ch264 <christinavhastenrath@gmail.com>
2019-07-26 15:50:53 -0700

    react app running on node server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 
-	console.log('hi');
 
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootNode = document.querySelector('#root');
+
+const App = () => {
+	return (
+		<div>
+			finally!
+		</div>
+	)
+
+}
+
+ReactDOM.render(<App />, rootNode)
